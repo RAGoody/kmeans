16ea0dba9215cea6afbc1c435744489a9cc80ca0
RAGoody <ryan.a.goodwin@gmail.com>
2026-08-05 15:34:31 -0400

    updated write() method with two optional parameters format and headers. write() method also updated with handling to write a CSV from a list if format is noted as CSV and data parameter is a list.

--- kmeans/file.py
+++ kmeans/file.py
@@ -5,15 +5,35 @@ class file:
     name = ""
     path = ""
     fullPath = ""
+    fileExists = False
     def __init__(self, path, name):
         self.name = name
         self.path = path
         self.fullPath = os.path.join(self.path, self.name)
         if not os.path.isfile(self.fullPath):
-            raise FileNotFoundError(f"File '{self.name}' does not exist.")
+            print(f"File '{self.name}' does not exist.")
+        else:
+            self.fileExists = True
+            print(f"File '{self.name}' exists.")
     def read(self):
-        with open(self.fullPath, 'r') as f:
-            return f.read()
-    def write(self, data):
-        with open(self.fullPath, 'w') as f:
-            f.write(data)
+        if self.fileExists == True:
+            with open(self.fullPath, 'r') as f:
+                return f.read()
+        else:
+            raise FileNotFoundError(f"File '{self.name}' does not exist.")  
+    def write(self, data, format='',headers=''):
+        match format:
+            case 'csv':
+                with open(self.fullPath, 'a') as f:
+                    if headers:
+                        f.write(f"{headers}\n")
+                    for row in data:
+                        csvRow = ''.join(str(value) for value in row)
+                        f.write(csvRow)
+                        f.write("\n")
+            case 'json':
+                with open(self.fullPath, 'w') as f:
+                    f.write(data)
+            case _:
+                with open(self.fullPath, 'w') as f:
+                    f.write(data)
